9a438e5619333896db290b1b6ceb9e36efa3eb64
DGesto <dianagesto@gmail.com>
2025-01-23 21:21:52 +0100

    Player is draw on screen and can be rotated with A and D.)

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 ### MODULES ###
 ###############
 import pygame
-# Imports the game constants from the constants.py file
 import constants
+import player
 
 
 #####################
@@ -17,6 +17,15 @@ def main():
     # Create a new GUI window
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     
+    # Create a game clock
+    clock = pygame.time.Clock()
+    dt = 0 # time difference
+    
+    # Initiate Player Object
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    player1 = player.Player(x, y)
+    
     # Game loop
     while True:
         # If close button is pressed, close the window
@@ -25,7 +34,12 @@ def main():
                 return
     
         screen.fill((0, 0, 0)) # fills the window in black
+        
+        player1.draw(screen)
+        player1.update(dt)
+        
         pygame.display.flip() # Refreshes the screen
+        dt = clock.tick(60) / 1000 # runs the game at 60 fps
         
     
     # print("Starting asteoids!")
